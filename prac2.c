bbbea241af9f822c6432d49f661015540d831cde
JG <garcia.jesus@uabc.edu.mx>
2021-02-11 18:01:21 -0800

    Seed files

--- prac2.c
+++ prac2.c
@@ -8,17 +8,10 @@ void puts(char *str);
 void itoa(unsigned int number, char* str, unsigned char base);
 unsigned int atoi(char *str);
 
-typedef struct bullsAndCowsTag{
-    unsigned bulls:4;
-    unsigned cows:4;
-} bullsAndCowsType;
-
-bullsAndCowsType evalBullsAndCows(char *secrete, char *guess);
-
 #ifndef TEST_HARNESS
 int main(void)
 {
-    // Port Prac1 to achieve same behavior
+    // Insert Code here
     return 0;
 }
 #endif // main()
